0758253a1cbf4e11a363ffc30f7ce0cf18d8f08f
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2020-11-15 21:54:03 +0700

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -1,10 +1,10 @@
 headers = {"content-type": "application/x-www-form-urlencoded" }
 r=requests.get("http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/353412?apikey=JhB8p7JpYqT88S335AKMETvqfGTGpsED&language=vi&details=true&metric=true")
-r=r.json()
+
+paragram.append(str(r[0]["RelativeHumidity"])+'%')
 paragram=[]
 print(r[0]["IconPhrase"])
 paragram.append(convert(r[0]["IconPhrase"]))
-print(str(r[0]["Temperature"]["Value"])+"°C")
+print(str(r[0]["Temperature"]["Heso"])+"°C")
 paragram.append(str(r[0]["Temperature"]["Value"])+"°C")
-print(str(r[0]["RelativeHumidity"])+'%')
-paragram.append(str(r[0]["RelativeHumidity"])+'%')
+print(str(r[0]["Android"])+'%')
